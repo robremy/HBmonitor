b1701cdcdf747acece312e178f7d5d2f57fbc11c
robremy <robremy@gmail.com>
2026-08-19 01:29:54 +0200

    Update HBmonitor

--- sw.js
+++ sw.js
@@ -5,16 +5,16 @@
  * Gebruik in index.html dezelfde SOFTWARE_VERSION.
  */
 
-const APP_VERSION = "2026.08.19-v47";
+const APP_VERSION = "2026.08.15-v43";
 const CACHE_PREFIX = "heart-rate-alert-cache-";
 const CACHE_NAME = CACHE_PREFIX + APP_VERSION;
-const OFFLINE_PAGE = "./index.html?v=2026.08.19-v47";
+const OFFLINE_PAGE = "./index.html?v=2026.08.15-v41";
 
 const APP_FILES = [
   "./",
-  "./index.html?v=2026.08.19-v47",
-  "./features.js?v=2026.08.19-v47",
-  "./manifest.webmanifest?v=2026.08.19-v47",
+  "./index.html?v=2026.08.15-v41",
+  "./features.js?v=2026.08.15-v41",
+  "./manifest.webmanifest?v=2026.08.15-v41",
   "./icon-192.png",
   "./icon-512.png"
 ];
